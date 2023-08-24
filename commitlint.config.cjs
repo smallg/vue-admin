40475f11e62986091a795db3e889cb4f110e498e
smallg <gwpxjb@163.com>
2023-08-24 23:46:55 +0800

    N/A: Add element plus and auto import

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['Feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']],
+    'type-enum': [2, 'always', ['Feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', '']],
     'subject-case': [0],
     'type-case': [2, 'always', ['pascal-case', 'upper-case']],
   },
